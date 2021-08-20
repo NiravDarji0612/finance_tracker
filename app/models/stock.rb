1eaa61705a18feb96255895aa22fa1bb230aad99
NiravDarji0612 <darjinirav18@gmail.com>
2021-08-20 19:03:50 +0530

    adding stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,8 @@
 class Stock < ApplicationRecord
+    
+   def new_lookup(ticker_symbol)
+      client = IEX::Api::Client.new(publishable_token: 'Tpk_c0fb046d3204491b9a84d0f206538a3d',
+                                    secret_token: 'Tsk_2a6104d52a0e4bd1ba487bb960044434',
+                                    endpoint: 'https://sandbox.iexapis.com/v1')
+   end
 end
