3c31324f0c9882d355b3d5da606befaf9367a2c2
NiravDarji0612 <darjinirav18@gmail.com>
2021-08-23 19:04:52 +0530

    adding front-end part of stock api

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,9 @@
 class Stock < ApplicationRecord
     
-   def new_lookup(ticker_symbol)
-      client = IEX::Api::Client.new(publishable_token: 'Tpk_c0fb046d3204491b9a84d0f206538a3d',
-                                    secret_token: 'Tsk_2a6104d52a0e4bd1ba487bb960044434',
+   def self.new_lookup(ticker_symbol)
+      client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:publishable_key],
+                                    secret_token: Rails.application.credentials.iex_client[:secret_key] ,
                                     endpoint: 'https://sandbox.iexapis.com/v1')
+      client.price(ticker_symbol)
    end
 end
